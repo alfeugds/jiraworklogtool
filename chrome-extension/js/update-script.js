3b51b9cb4eedb49201115737928904ef35c929bf
Alfeu Santos <alfeugds@gmail.com>
2017-12-22 00:50:51 -0200

    unit test updates

--- chrome-extension/js/update-script.js
+++ chrome-extension/js/update-script.js
@@ -1,8 +1,30 @@
 /* global chrome */
 var updateScript = (function(){
+    function saveOptions(options) {
+        chrome.storage.sync.set(
+            {
+                jiraOptions: options
+            },
+            function () {
+                Promise.resolve();
+            }
+        );
+    }
+    function getOptions() {
+        chrome.storage.sync.get(
+            {
+                jiraOptions: {}
+            },
+            function (items) {
+                Promise.resolve(items.jiraOptions);
+            }
+        );
+    }
+    function removePassword() {
+
+    }
     return {
         run: () => {
-            //console.log('update script start');
             // Check whether new version is installed
             chrome.runtime.onInstalled.addListener(function(details){
                 console.log('details', details);
@@ -11,6 +33,7 @@ var updateScript = (function(){
                 }else if(details.reason == "update"){
                     var thisVersion = chrome.runtime.getManifest().version;
                     console.log("Updated from " + details.previousVersion + " to " + thisVersion + "!");
+                    removePassword();
                 }
             });
             return Promise.resolve();
@@ -19,4 +42,4 @@ var updateScript = (function(){
 })();
 
 if (typeof module !== 'undefined')
-module.exports = updateScript;
+    module.exports = updateScript;
--- js/tests/update-script.spec.js
+++ js/tests/update-script.spec.js
@@ -1,9 +1,9 @@
+/* global test jest expect*/
 const updateScript = require("../../chrome-extension/js/update-script");
 
-let window = {};
-
 test("should run update storage script upon extension update", (done) => {  
-    var isCalled = false;
+    //arrange
+    let isCalled = false;
     global.chrome = {
         runtime: {
             onInstalled: {
@@ -25,14 +25,20 @@ test("should run update storage script upon extension update", (done) => {
     global.console = {
         log: jest.fn()
     };
-    updateScript.run();
-    expect(chrome.runtime.onInstalled.addListener).toHaveBeenCalled();
-    expect(isCalled).toBeTruthy();
-    expect(console.log).toHaveBeenCalledWith('Updated from 0.2.2 to 0.2.3!');
-    expect(console.log).toHaveBeenCalledWith('details', {
-            reason: 'update',
-            previousVersion: '0.2.2'
-        });
-    expect(chrome.runtime.getManifest).toHaveBeenCalled();
-    done();
+    let chrome = global.chrome;
+
+    //act
+    updateScript.run().then(() => {
+        //assert
+        expect(chrome.runtime.onInstalled.addListener).toHaveBeenCalled();
+        expect(isCalled).toBeTruthy();
+        expect(console.log).toHaveBeenCalledWith('Updated from 0.2.2 to 0.2.3!');
+        expect(console.log).toHaveBeenCalledWith('details', {
+                reason: 'update',
+                previousVersion: '0.2.2'
+            });
+        expect(chrome.runtime.getManifest).toHaveBeenCalled();
+        done();
+    });
+
 });
