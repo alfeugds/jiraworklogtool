9c35ff9a4316e77de40ed4fe322352e9bdb1e0a5
Alfeu Lucas Guedes dos Santos <alfeu@ciandt.com>
2019-02-26 18:35:30 -0300

    fixed jest config files

--- jest.config.integration.js
+++
@@ -1,4 +0,0 @@
-var config = require('jest-config')
-config.testRegex = 'int.spec.js'
-
-module.exports = config
--- jest.config.unit.js
+++
@@ -1,4 +0,0 @@
-var config = require('jest-config')
-config.testRegex = 'unit.spec.js'
-
-module.exports = config
